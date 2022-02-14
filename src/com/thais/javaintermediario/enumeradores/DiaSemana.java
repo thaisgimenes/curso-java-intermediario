5bd75d00e7762d58c6089cd9f01014df0071acf0
[thaisgimenes] <[thaisgimenes@outlook.com.br]>
2022-02-14 19:53:23 -0300

    Aula 1 - Enumeradores(Enum)

---
+++ src/com/thais/javaintermediario/enumeradores/DiaSemana.java
@@ -0,0 +1,8 @@
+package com.thais.javaintermediario.enumeradores;
+
+public enum DiaSemana {
+	
+	//Criando enums para representar dias da semana;
+	
+	SEGUNDA, TERCA, QUARTA, QUINTA, SEXTA, SABADO, DOMINGO;
+}
