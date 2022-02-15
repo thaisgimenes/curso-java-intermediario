2135fe510ad2e96b7edeba65771b75cb3bb8904d
[thaisgimenes] <[thaisgimenes@outlook.com.br]>
2022-02-15 10:04:03 -0300

    Enum: metodos abstratos

---
+++ src/com/thais/javaintermediario/enumeradores/TipoDocumento.java
@@ -0,0 +1,17 @@
+package com.thais.javaintermediario.enumeradores;
+
+public enum TipoDocumento {
+
+	CPF {
+		@Override
+		public String geraNumeroTeste() {
+			return GeraCpfCnpj.cpf();
+		}
+	}, CNPJ {
+		@Override
+		public String geraNumeroTeste() {
+			return GeraCpfCnpj.cnpj();		}
+	};
+	
+	public abstract String geraNumeroTeste();
+}
